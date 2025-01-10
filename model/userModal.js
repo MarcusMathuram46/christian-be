471a78ad2ea53777e658aefee5e87ec9ca485374
Marcus Mathuram <marcus.mathuram7@gmail.com>
2025-01-10 22:01:41 +0530

    Done

--- model/userModal.js
+++ model/userModal.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date },
-  },
-  { versionKey: false }
-);
+const userSchema = new mongoose.Schema({
+    name:{ type: String, required: true },
+    email:{ type: String, required: true, unique: true },
+    passwordHash:{ type: String, required: true },
+    createdAt:{ type: Date, default: Date.now },
+    updatedAt:{ type: Date }
+},{versionKey: false})
+
 
 const User = mongoose.model("User", userSchema);
 
